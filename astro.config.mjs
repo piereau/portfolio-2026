2522de9dc8c6c34908b5f3ca79723b37fcab5ae6
piereau <pierreolivier.rouxsavelli@edu.ece.fr>
2026-04-15 19:41:00 +0200

    remove astro template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://piereau.github.io',
   base: '/portfolio-2026',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
